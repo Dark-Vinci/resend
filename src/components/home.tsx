f61c8ceb2e284615e7edd062d7342f6895572795
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-03 20:03:33 +0100

    chore: more on home

---
+++ src/components/home.tsx
@@ -0,0 +1,11 @@
+import React, { JSX } from 'react';
+
+import style from './home.module.scss';
+
+export function Home(): JSX.Element {
+    return (
+        <div className={style.container}>
+
+        </div>
+    );
+}
