e532c288f12a109bc9068964cdc7ca7a352bb3cf
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2023-12-03 23:08:30 +0100

    chore: nice nice

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -4,7 +4,30 @@ import style from './nav.module.scss';
 export function Navigation(): JSX.Element {
     return (
         <div className={style.container}>
+            <div className={style.logo}>
 
+            </div>
+
+            <div className={style.nav}>
+                <div className={style.nav_container}>
+                    <ul>
+                        <li><a href="."/>about</li>
+                        <li><a href="."/>blog</li>
+                        <li><a href="."/>customers</li>
+                        <li><a href="."/>Pricing</li>
+                        <li><a href="."/>Enterprise</li>
+                        <li><a href="."/>Changelog</li>
+                        <li><a href="."/>Docs</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className={style.right}>
+                <div className={style.right_container}>
+                    <button>Sign in</button>
+                    <button>Get Started</button>
+                </div>
+            </div>
         </div>
     );
 }
